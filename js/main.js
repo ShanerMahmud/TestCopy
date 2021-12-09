ec596b5927c0dee1dbe7d1109b20d60f3ac9cd59
ShanerMahmud <0265346@student.rocmondriaan.nl>
2021-12-09 11:50:00 +0100

    Centered DIV's and changed CSS a bit to PRACTICE

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@
 // Dit is commentaar code, dit wordt niet uitgevoerd maar is voor developers
 
 //Onderstaande code toont een alert box, verander de tekst eens in de alert box
-alert("Hello World");
+// alert("Hello World");
+// 
